72a03b7d01eaff4f52edf1bb93657c78ea62bb6b
Maulana Mudhoffar <Laila Azzahra Achmad>
2026-09-15 12:50:28 +0700

    menambahkan program

---
+++ coding/PemilihanHariDenganif15.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PemilihanHariDenganif15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input a number (1-7): ");
+        int dayNumber = sc.nextInt();
+        String dayType;
+
+        if (dayNumber >= 1 && dayNumber <= 5) {
+            dayType = "Weekday";
+        }else if (dayNumber == 6 || dayNumber == 7) {
+            dayType = "Weekend";
+        }else {
+            dayType = "Invalid Number";
+        }
+
+        System.out.println(dayType);
+
+        sc.close();
+    }
+}
